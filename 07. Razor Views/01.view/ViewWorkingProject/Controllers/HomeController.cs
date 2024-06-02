1b0c8ccdd9c0cbe215ad85108bff810ba203c2e9
Krutarth88 <42194330+Krutarth88@users.noreply.github.com>
2024-06-02 10:09:11 +0530

    inital
    Push Project

---
+++ 07. Razor Views/01.view/ViewWorkingProject/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewWorkingProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View(); //Views/Home/Index.cshtml
+            //return new ViewResult() { ViewName = "abc" };
+        }
+    }
+}
